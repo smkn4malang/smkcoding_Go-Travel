4697cefad27154640fb43f066b13e2c77a98263d
rizqisatriaw <rizqisatriaw191@gmail.com>
2018-10-30 20:19:26 +0700

    Top-Up + Pembayaran

---
+++ app/src/main/java/com/rizqisatria/go_travel/Bukti_Activity.java
@@ -0,0 +1,13 @@
+package com.rizqisatria.go_travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bukti_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bukti_);
+    }
+}
